1022e268e511164f2a2d65033230203522e8d711
LeeJiHyeon <rachel2148072@gmail.com>
2023-03-02 22:22:55 +0900

    7.7,7.8 :express-session 이해하기

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import session from "express-session";
 
 const app = express();
 const logger = morgan("dev");
@@ -13,6 +14,25 @@ app.use(logger);
 //여기 이 지점에는  request.body === undefined
 app.use(express.urlencoded({ extended: true })); // application/x-www-form-urlencoded파싱 (form데이터 파싱) : 미들웨어는 라우터 전에 작성되어야함 (videoRouter을 겨냥)
 // request.body 존재 하기 시작 - videoRouter에서 post 요청보낸 내용이 담겨져있음
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+
+app.get("/add-one", (req, res, next) => {
+  req.session.potato += 1;
+  return res.send(`${req.session.id} ${req.session.potato}`);
+});
 
 app.use("/", rootRouter);
 app.use("/users", userRouter);
