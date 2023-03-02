9e9fa3e206656c573f9146fba6bce14b92813f7c
LeeJiHyeon <rachel2148072@gmail.com>
2023-03-03 01:08:31 +0900

    7.9~7.11 : sessionID와 cookie의 차이. res.locals 객체로 view에 렌더링하기

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -58,7 +58,8 @@ export const postLogin = async (req, res) => {
       pageTitle: "Login",
       errorMessage: "Wrong password.",
     });
-
+  req.session.loggedIn = true;
+  req.session.user = user;
   return res.redirect("/");
 };
 
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 import session from "express-session";
+import { localsMiddleware } from "./routers/middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -22,18 +23,7 @@ app.use(
     saveUninitialized: true,
   })
 );
-app.use((req, res, next) => {
-  req.sessionStore.all((error, sessions) => {
-    console.log(sessions);
-    next();
-  });
-});
-
-app.get("/add-one", (req, res, next) => {
-  req.session.potato += 1;
-  return res.send(`${req.session.id} ${req.session.potato}`);
-});
-
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
