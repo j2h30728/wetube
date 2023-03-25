de845c6dd310e56cfd4048ae469ea25f9e85f57a
LeeJiHyeon <rachel2148072@gmail.com>
2023-03-25 19:30:02 +0900

    14.1 : ffpmg.wasm 에러 처리2:프로필사진에러 해결

--- src/server.js
+++ src/server.js
@@ -10,20 +10,21 @@ import apiRouter from "./routers/apiRouter";
 
 const app = express();
 const logger = morgan("dev");
-
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+
+//Uncaught (in promise) ReferenceError: SharedArrayBuffer is not defined 오류 해결 방법
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Embedder-Policy", "credentialless");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
+
 app.use(logger);
 //여기 이 지점에는  request.body === undefined
 app.use(express.urlencoded({ extended: true })); // application/x-www-form-urlencoded파싱 (form데이터 파싱) : 미들웨어는 라우터 전에 작성되어야함 (videoRouter을 겨냥)
 // request.body 존재 하기 시작 - videoRouter에서 post 요청보낸 내용이 담겨져있음
 
-//Uncaught (in promise) ReferenceError: SharedArrayBuffer is not defined 오류 해결 방법
-app.use((req, res, next) => {
-  res.header("Cross-Origin-Embedder-Policy", "require-corp");
-  res.header("Cross-Origin-Opener-Policy", "same-origin");
-  next();
-});
 app.use(
   session({
     secret: process.env.COOKIE_SECRET,
